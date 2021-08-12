4b794006d38fc5f4f51937286238c5cbc58bc472
Anthony Puitiza <Anthony.Puitiza@nautilus.team>
2021-08-12 15:26:26 -0500

    First init commit

---
+++ src/main/java/io/reflectoring/exception/entity/Product.java
@@ -0,0 +1,42 @@
+package io.reflectoring.exception.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    private String name;
+
+    private Double price;
+
+    private LocalDateTime manufacturingDate;
+
+
+    private Double weight;
+
+    @Embedded
+    private Dimension dimension;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    @Embeddable
+    @Getter
+    @Setter
+    public static class Dimension {
+        private Double height;
+
+        private Double width;
+    }
+
+
+}
